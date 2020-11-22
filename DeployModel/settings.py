8274b9df3caef8e430925655f66b08372fedd50f
MRajibH <rajib.ndc.ewu@gmail.com>
2020-11-22 19:05:59 +0600

    eibar kaj korish vai

--- DeployModel/settings.py
+++ DeployModel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'afhw+_@%(4lef9cm94t(s62@5425f#(ny1ue@xof#@v@gg)!ib'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'suicide-risk-checker.herokuapp.com']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,5 +117,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
