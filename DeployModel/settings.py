679e284600c22c1ceac50f908fbb544d9f7999f5
MRajibH <rajib.ndc.ewu@gmail.com>
2020-11-23 00:10:51 +0600

    one last time

--- DeployModel/settings.py
+++ DeployModel/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'afhw+_@%(4lef9cm94t(s62@5425f#(ny1ue@xof#@v@gg)!ib'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'suicide-risk-checker.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', ['suicide-risk-checker.heroku.com']]
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DeployModel',
 ]
 
 MIDDLEWARE = [
@@ -116,14 +117,5 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
